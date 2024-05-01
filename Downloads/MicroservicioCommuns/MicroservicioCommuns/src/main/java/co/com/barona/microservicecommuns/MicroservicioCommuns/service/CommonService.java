4aa80250c6cea920ca309383557ce39652b691f5
barona13 <baronaa13@hotmail.com>
2024-05-01 01:17:52 -0500

    servicios Getway y commons

---
+++ Downloads/MicroservicioCommuns/MicroservicioCommuns/src/main/java/co/com/barona/microservicecommuns/MicroservicioCommuns/service/CommonService.java
@@ -0,0 +1,16 @@
+package co.com.barona.microservicecommuns.MicroservicioCommuns.service;
+
+
+
+import java.util.Optional;
+
+public interface CommonService<E>{
+
+    public Iterable<E> findAll();
+
+    public Optional<E> findById(Long id);
+
+    public E save(E entity);
+
+    public void  deleteById (Long id);
+}
---
+++ Downloads/MicroservicioCommuns/MicroservicioCommuns/src/test/java/co/com/barona/microservicecommuns/MicroservicioCommuns/MicroservicioCommunsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.barona.microservicecommuns.MicroservicioCommuns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioCommunsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
